4b14c0d7775e579f762ef420a97e1b4253ad8fab
wootae <comijoa2@naver.com>
2026-01-10 16:10:11 +0900

    test: 카트 주문 테스트 코드 작성

--- src/test/java/com/commerce/service/PayServiceTest.java
+++ src/test/java/com/commerce/service/PayServiceTest.java
@@ -3,8 +3,11 @@ package com.commerce.service;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,10 +29,13 @@ import com.commerce.config.IntegrationTest;
 import com.commerce.domain.Orders;
 import com.commerce.domain.Product;
 import com.commerce.domain.User;
+import com.commerce.domain.enums.OrderStatus;
 import com.commerce.domain.enums.OrderType;
 import com.commerce.domain.enums.RoleType;
 import com.commerce.dto.OrderCreateRequestDTO;
 import com.commerce.dto.PayConfirmDTO;
+import com.commerce.repository.CartProductRepository;
+import com.commerce.repository.CartRepository;
 import com.commerce.repository.OrderProductRepository;
 import com.commerce.repository.OrderRepository;
 import com.commerce.repository.ProductRepository;
@@ -64,6 +70,12 @@ class PayServiceTest {
 	@MockitoBean SecurityUtil securityUtil;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private CartService cartService;
+	@Autowired
+	private CartRepository cartRepository;
+	@Autowired
+	private CartProductRepository cartProductRepository;
 
 	@BeforeEach
 	void stubs() {
@@ -84,8 +96,70 @@ class PayServiceTest {
 	void cleanUp() {
 		orderProductRepository.deleteAllInBatch(); // orders, product를 물고 있음(자식)
 		orderRepository.deleteAllInBatch();        // user를 물고 있음(자식)
+		cartProductRepository.deleteAllInBatch();
+		cartRepository.deleteAllInBatch();
 		productRepository.deleteAllInBatch();      // 더 이상 참조 없을 때
 		userRepository.deleteAllInBatch();         // orders가 먼저 삭제되어야 안전
+	}
+
+	@Test
+	@DisplayName("pay confirm 카트 주문 테스트")
+	void payConfirmByCartOrder() {
+		// given
+		// 사용자 생성
+		User user = User.builder()
+			.customerPaymentKey("customerPaymentKey")
+			.email("c@naver.com")
+			.name("홍길동")
+			.phone("01012345678")
+			.role(RoleType.ROLE_USER)
+			.username("username")
+			.build();
+		user = userRepository.save(user);
+
+		Mockito.when(securityUtil.getCurrentUser()).thenReturn(user);
+
+		int stock = 100;
+		int quantity = 1;
+
+		// 상품 생성
+		Product product = new Product();
+		product.update(1000, stock, "상품1", "설명");
+		product = productRepository.save(product);
+
+		// 카트 담기
+		cartService.addCart(product.getId(), quantity);
+
+		// 주문 준비
+		OrderCreateRequestDTO dto = new OrderCreateRequestDTO();
+		dto.setName("홍길동");
+		dto.setPhone("01012345678");
+		dto.setAddress("서울");
+		dto.setAddressDetail("어딘가");
+		dto.setOrderType(OrderType.CART);
+		dto.setProductId(product.getId());
+		dto.setQuantity(quantity);
+
+		Orders orders = orderService.prepareOrderFromBuyNow(dto);
+
+		String orderNumber = orders.getOrderNumber();
+		String paymentKey = UUID.randomUUID().toString();
+		int amount = orders.getFinalPrice();
+
+
+
+		PayConfirmDTO payConfirmDTO = new PayConfirmDTO(paymentKey, orderNumber, amount);
+
+		// when
+		payService.confirm(payConfirmDTO,user.getId());
+
+		// then
+		Product afterProduct = productRepository.findById(product.getId()).orElseThrow();
+		Orders afterOrder = orderRepository.findByOrderNumber(orderNumber).orElseThrow();
+
+		assertThat(afterProduct.getStock()).isEqualTo(stock - quantity);
+		assertThat(afterOrder.getOrderStatus()).isEqualTo(OrderStatus.PAID);
+		assertThat(afterOrder.getApprovedAt()).isEqualTo(LocalDateTime.of(2026, 1, 3, 1, 23, 45));
 	}
 
 
@@ -166,9 +240,16 @@ class PayServiceTest {
 		// then
 		Product product1 = productRepository.findById(product.getId())
 			.orElseThrow();
+		List<Orders> all = orderRepository.findAll();
 		assertThat(finished).isTrue();
 		assertThat(fail.get()).isEqualTo(0);
 		assertThat(product1.getStock()).isEqualTo(50);
+
+		for (Orders orders : all) {
+			assertThat(orders.getOrderStatus()).isEqualTo(OrderStatus.PAID);
+			assertThat(orders.getApprovedAt()).isEqualTo(LocalDateTime.of(2026, 1, 3, 1, 23, 45));
+		}
+
 	}
 
 }
